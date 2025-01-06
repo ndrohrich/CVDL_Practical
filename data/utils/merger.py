bbcf8f45fadb813046de28601dcabf672381a7ca
logos <yuanluowu@gmail.com>
2025-01-06 12:04:10 +0100

    docs: update dataset selection comments and add note on class differences between FER2013 and CK+ datasets

--- data/utils/merger.py
+++ data/utils/merger.py
@@ -22,9 +22,9 @@ def merge_all_datasets(cfg, train_transforms, test_transforms):
     # train_dataset = torch.utils.data.ConcatDataset([datasets[dataset](train_transforms, 'train') for dataset in datasets])
     # test_dataset = torch.utils.data.ConcatDataset([datasets[dataset](test_transforms, 'test') for dataset in datasets])
     
-    #using ckplus as dataset
-    train_dataset = CK_plus(train_transforms, 'train')
-    test_dataset = CK_plus(test_transforms, 'test')
+    # #using ckplus as dataset
+    # train_dataset = CK_plus(train_transforms, 'train')
+    # test_dataset = CK_plus(test_transforms, 'test')
     
     #using fer2013 as dataset
     train_dataset = FER2013(train_transforms, 'train')
@@ -42,6 +42,7 @@ def merge_all_datasets(cfg, train_transforms, test_transforms):
 def select_dataset(cfg, train_transforms, test_transforms):
     '''
     Selects dataset based on cfg.dataset and returns train and test splits. 
+    Note: FER2013 dataset has 7 classes and CK+ dataset has 6 classes. please set the right number of classes in the config file.
 
     Parameters
     ----------

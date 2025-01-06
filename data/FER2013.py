2ef74a459a1b4e1c45c310105129808f83021eab
logos <yuanluowu@gmail.com>
2025-01-06 12:56:15 +0100

    fix: update dataset mode in config and comment out unused code in FER2013.py

--- data/FER2013.py
+++ data/FER2013.py
@@ -115,15 +115,15 @@ class FER2013(Dataset):
     
         
     
-from utils import augmentations
+# from utils import augmentations
 
-if __name__=='__main__':
-    transforms=augmentations.init_transforms(None)
-    dataset=FER2013(transform=transforms['train'])
+# if __name__=='__main__':
+#     transforms=augmentations.init_transforms(None)
+#     dataset=FER2013(transform=transforms['train'])
     
-    print(len(dataset))
-    img,target=dataset[np.random.randint(0,len(dataset))]
-    print(img.shape)
-    print(target)
-    plt.imshow(img[0])
-    plt.show()
+#     print(len(dataset))
+#     img,target=dataset[np.random.randint(0,len(dataset))]
+#     print(img.shape)
+#     print(target)
+#     plt.imshow(img[0])
+#     plt.show()

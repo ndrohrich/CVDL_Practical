a1d61ea74a99f38e1a02e3ecc9a91740cb362302
Yuanluo_Wu <yuanluowu@gmail.com>
2025-01-15 23:03:54 +0100

    added new transformation function with brightness and contrast adjustments

--- data/utils/augmentations.py
+++ data/utils/augmentations.py
@@ -1,4 +1,5 @@
 from torchvision import transforms as tf
+import numpy as np
 
 def init_transforms(cfg): 
     '''
@@ -90,6 +91,7 @@ def randomErasing_flip_rotate_transforms(cfg):
     train_transforms = tf.Compose([tf.Resize((64,64)),
                                    tf.Grayscale(),
                                    tf.RandomHorizontalFlip(p=cfg.probability),
+                                   tf.RandomRotation(degrees=cfg.rotation_angle),
                                    tf.ToTensor(),
                                    tf.RandomErasing(cfg.probability, scale=(cfg.min_area, cfg.max_area), ratio=(cfg.min_aspect_ratio, cfg.max_aspect_ratio))])
     test_transforms = tf.Compose([tf.Resize((64,64)),
@@ -100,3 +102,32 @@ def randomErasing_flip_rotate_transforms(cfg):
                   'test': test_transforms}
     
     return transforms
+
+def random_Erasing_flip_Brightness_Contrast_transforms(cfg):
+    '''
+    Takes arguments specified in command line via hydra and returns 
+    train and test transforms using torch transforms. 
+
+    Parameters
+    ----------
+
+    cfg: OmegaConf
+        Hyperparameters parsed via command-line using hydra.
+    '''
+    
+    train_transforms = tf.Compose([tf.Resize((64,64)),
+                                   tf.Grayscale(),
+                                   tf.RandomHorizontalFlip(p=cfg.probability),
+                                   tf.ColorJitter(brightness=np.random.uniform(cfg.brightness[0], cfg.brightness[1]), contrast=np.random.uniform(cfg.contrast[0], cfg.contrast[1])),
+                                   tf.ToTensor(),
+                                   tf.RandomErasing(cfg.probability, scale=(cfg.min_area, cfg.max_area), ratio=(cfg.min_aspect_ratio, cfg.max_aspect_ratio))])
+    test_transforms = tf.Compose([tf.Resize((64,64)),
+                                  tf.Grayscale(),
+                                  tf.ToTensor()])
+
+    transforms = {'train': train_transforms, 
+                  'test': test_transforms}
+    
+    return transforms
+
+

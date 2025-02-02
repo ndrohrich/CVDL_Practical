softmax fix, added half masking augments, set augments true by default

data/utils/augmentations.py
@@ -15,10 +15,19 @@ def init_transforms(cfg):
     cfg: OmegaConf
         Hyperparameters parsed via command-line using hydra.
     '''
-    
-    train_transforms = tf.Compose([tf.Resize((64,64)),
+    if cfg.augments:
+        train_transforms = tf.Compose([tf.Resize((64,64)),
                                    tf.Grayscale(),
-                                   tf.ToTensor()])
+                                   tf.RandomHorizontalFlip(p=cfg.probability),
+                                   tf.ColorJitter(brightness=np.random.uniform(cfg.brightness[0], cfg.brightness[1]), contrast=np.random.uniform(cfg.contrast[0], cfg.contrast[1])),
+                                   tf.ToTensor(),
+                                   tf.RandomErasing(cfg.probability*0.5, scale=(cfg.min_area, cfg.max_area), ratio=(cfg.min_aspect_ratio, cfg.max_aspect_ratio)),
+                                   tf.Lambda(lambda x: random_half_black(x,cfg.probability*0.5))])
+    else:
+        train_transforms = tf.Compose([tf.Resize((64,64)),
+                                    tf.Grayscale(),
+                                    tf.ToTensor()])
+    
     test_transforms = tf.Compose([tf.Resize((64,64)),
                                   tf.Grayscale(),
                                   tf.ToTensor()])

models/Hybrid_Simple_Stack.py
@@ -77,7 +77,7 @@ class Hybrid(nn.Module):
             cam = F.relu(cam) '''
 
         self.logits = output
-        return F.softmax(output, dim=1)
+        return output
 
 # CNN Encoder returns outputs of shape [batch, 256, 16, 16] given [batch, 1, 64, 64] input images
 class CNN_encoder(nn.Module): 
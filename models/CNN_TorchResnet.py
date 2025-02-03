Softmax fix for VGG and Torch Resnet

models/CNN_TorchResnet.py
@@ -27,6 +27,10 @@ class TorchVisionResNet(nn.Module):
         
         self.model.fc = nn.Linear(self.model.fc.in_features, num_classes)
 
-    def forward(self, x):
+    def forward(self, x, apply_softmax = False):
         logits = self.model(x)
+
+        if apply_softmax:
+            logits = F.softmax(logits, dim=1)
+            
         return logits

models/CNN_VGG.py
@@ -94,7 +94,7 @@ class VGG(nn.Module):
 
         
 
-    def forward(self, x):
+    def forward(self, x, apply_softmax = False):
         x = self.layer1(x)
         x = self.layer2(x)
         x = self.layer3(x)
@@ -113,5 +113,8 @@ class VGG(nn.Module):
         x = self.fc(x)
         x=self.fc1(x)
         x=self.fc2(x)
+
+        if apply_softmax:
+            x=nn.functional.softmax(x, dim=1)
 
         return x